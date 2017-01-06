9117c6db09852c2e048ca510f6a2297fdb752477
Kenny GUIOUGOU <kenny.guiougou@gmail.com>
2017-01-06 01:37:09 +0100

    [Insight] Missing use statement should be avoided

--- src/AppBundle/Controller/SummonerAjaxController.php
+++ src/AppBundle/Controller/SummonerAjaxController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class SummonerAjaxController extends Controller
 {
