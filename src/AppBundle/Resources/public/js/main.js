9056bcc52c9acb50f5975d9eccb74cba9e05475e
Kenny GUIOUGOU <kenny.guiougou@gmail.com>
2017-03-07 21:25:32 +0100

    correction navbar

---
+++ src/AppBundle/Resources/public/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.region-div').click(function () {
+        var text = $(this).children().first().text();
+        $('#button-region-text').text(text.toUpperCase());
+        $('#searchbar-region').attr('value', text);
+        $('#modalChooseRegion').modal('hide');
+    });
+});
