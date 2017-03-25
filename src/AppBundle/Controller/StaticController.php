da803f3d3dc11d28e6131e059582318e814f231a
Kenny GUIOUGOU <kenny.guiougou@gmail.com>
2017-03-25 18:44:26 +0100

    ajout formulaire contact et factorisation des pages d'erreur

--- src/AppBundle/Controller/StaticController.php
+++ src/AppBundle/Controller/StaticController.php
@@ -5,6 +5,12 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class StaticController extends Controller
 {
@@ -21,6 +27,37 @@ class StaticController extends Controller
     public function aboutAction()
     {
         return $this->render('AppBundle:Static:about.html.twig');
+    }
+
+    public function contactAction(Request $request)
+    {
+        $defaultData = array('message' => 'Type your message here');
+        $form = $this->createFormBuilder($defaultData)
+            ->add('name', TextType::class, array(
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('min' => 2, 'max' => 20)))
+            ))
+            ->add('email', EmailType::class, array(
+                'constraints' => array(
+                    new NotBlank())
+            ))
+            ->add('message', TextareaType::class, array(
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('min' => 50, 'max' => 1000)))
+            ))
+            ->add('send', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+            $data = $form->getData();
+        }
+
+        return $this->render('AppBundle:Static:contact.html.twig', array('form' => $form->createView()));
     }
 
     public function searchbarAction(Request $request)
